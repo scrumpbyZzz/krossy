7571ab941d9e1bf3688b122e0dd01a14e820e6e7
zetorby <zetorby24@gmail.com>
2019-08-28 23:33:08 +0300

    fix fetch

--- src/panels/startPanel/startPanelOne/StartPanelOne.js
+++ src/panels/startPanel/startPanelOne/StartPanelOne.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Panel, ScreenSpinner } from '@vkontakte/vkui';
+import {Panel, ScreenSpinner} from '@vkontakte/vkui';
 import './StartPanelOne.css';
 import RectangleButton from "../../../components/buttons/rectangleButton/RectangleButton";
 import DotsSlide from "../../../components/dotsSlide/DotsSlide";
@@ -16,43 +16,40 @@ class StartPanelOne extends React.Component {
     this.props.isLoad(true);
     this.Service.loadSetting(id)
       .then(res => {
-        if(res.ok) {
-          this.props.gender(res.result.gender);
-          res.result.size.forEach(s => this.props.size(s));
-          this.props.isLoad(false);
-        }
+        //todo сделать проверку на загрузку юзера
+        this.props.gender(res.result.gender);
+        res.result.size.forEach(s => this.props.size(s));
+        this.props.isLoad(false);
       })
   };
 
   componentDidMount() {
-    if(!this.props.data.isLoadUserInfo) {
-     this.loadSettings();
+    if (!this.props.data.isLoadUserInfo) { // проверка на загругку данных их VK
+      this.loadSettings();
     }
   }
 
   render() {
-    const { data } = this.props;
+    const {data} = this.props;
     return (
-      data.isLoadSetting ?
-        <ScreenSpinner /> :
-      <Panel id={this.props.id} >
-        <div className='start-panel-one_wrap'>
-          <div className='start-panel-one_content'>
-            <div className='start-panel-one-content_title'>Кроссы</div>
-            <div className='start-panel-one-content_text'>
-              Сервис «Кроссы» - это отличный помощник в нелегкой задаче поиска своих самых любимых кросовок!
+        <Panel id={this.props.id}>
+          <div className='start-panel-one_wrap'>
+            <div className='start-panel-one_content'>
+              <div className='start-panel-one-content_title'>Кроссы</div>
+              <div className='start-panel-one-content_text'>
+                Сервис «Кроссы» - это отличный помощник в нелегкой задаче поиска своих самых любимых кросовок!
+              </div>
             </div>
+            <RectangleButton title='Далее'
+                             goTo='start-2'
+                             func={this.props.goPanel}
+            />
+            <DotsSlide/>
           </div>
-          <RectangleButton title='Далее'
-                           goTo='start-2'
-                           func={this.props.goPanel}
-                           />
-          <DotsSlide />
-        </div>
-      </Panel>
+        </Panel>
     )
   }
-};
+}
 
 export default reduxConnect(
   state => ({
